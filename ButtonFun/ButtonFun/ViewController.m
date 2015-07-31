96115eb1987c614eab1eef96d50e519749a5ef27
vipulanurag612 <vipulanurag4.p@gmail.com>
2015-07-31 01:52:42 -0400

    Added logic for duplicate color

--- ButtonFun/ButtonFun/ViewController.m
+++ ButtonFun/ButtonFun/ViewController.m
@@ -10,6 +10,7 @@
 #import "UIColor+RandomColor.h"
 
 NSString *kCellID = @"cellID";          //UICollectionViewCell storyBoard id
+#define kCellHieghtWidth 5
 
 @implementation ViewController
 
@@ -17,13 +18,21 @@ NSString *kCellID = @"cellID";          //UICollectionViewCell storyBoard id
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     
-    return 1000;         //Random value to cover screens of all Apple devices
+    return [self numberOfSquares];         //Random value to cover screens of all Apple devices
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     
     UICollectionViewCell *cell  = [collectionView dequeueReusableCellWithReuseIdentifier:kCellID forIndexPath:indexPath];
-    cell.backgroundColor = [UIColor RandomColor];  //RandomColor category added to provide ramdom colors
+    
+    UIColor *currentCellBackgroundColor = cell.backgroundColor;
+    UIColor *nextBackgroundColor = [UIColor RandomColor]; //RandomColor Category added to UIColor to provide ramdom colors
+    
+    /*varifying current cell background is different from the next one*/
+    if(nextBackgroundColor == currentCellBackgroundColor)
+        nextBackgroundColor = [UIColor RandomColor];
+    
+    cell.backgroundColor = nextBackgroundColor;
     
     return cell;
 }
@@ -32,6 +41,18 @@ NSString *kCellID = @"cellID";          //UICollectionViewCell storyBoard id
 {
     UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
     cell.backgroundColor = [UIColor RandomColor];
+}
+
+#pragma mark Private Methods
+
+-(NSInteger)numberOfSquares {
+    
+    CGFloat width = self.view.bounds.size.width;
+    NSInteger columns = width / kCellHieghtWidth;
+    CGFloat height = self.view.bounds.size.height;
+    NSInteger rows = height / kCellHieghtWidth;
+    
+    return columns * rows;
 }
 
 @end
